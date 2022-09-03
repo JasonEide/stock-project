8a881d41f224b24b19b05037bec85b3b80241847
JasonEide <jeide15@yahoo.com>
2022-09-03 12:37:13 -0400

    additional commit

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -12,13 +12,37 @@ export default function Cards(/*You guys add parameters here. just read the code
     */
    return (
     <div className={styles.container}> 
-        <Grid container spacing={3} justifyContent="center">
-            <Grid item component={Card} className={names(styles.card, styles.total /* HINT HINT styles.total is a parameter that must be added 
+        <Grid container spacing={2} justifyContent="center">
+            <Grid item component={Card} xs={5} md={5} className={names(styles.card, styles.total /* HINT HINT styles.total is a parameter that must be added 
             (total portfolio value, aka all the stocks within the stock portfolio added together)*/)}>
                 <CardContent>
                     <Typography color='textSecondary'>Portfolio Value</Typography> 
                     <Typography variant='h5'>
                         <CountUp start={0} end={1000000} /*Must change 1000000 to the portfolio value*/ duration={2.5} separator=','/>
+                    </Typography>
+                </CardContent>
+            </Grid>
+            <Grid item component={Card} xs={5} md={5} className={names(styles.card, styles.SECONDPARAMTERR)} /* SECOND COMPONENT */>
+                <CardContent>
+                    <Typography color='textSecondary'>SECOND THING</Typography> 
+                    <Typography variant='h5'>
+                        <CountUp start={0} end={1000000} duration={2.5} separator=','/>
+                    </Typography>
+                </CardContent>
+            </Grid>
+            <Grid item component={Card} xs={5} md={5} className={names(styles.card, styles.THIRDPARAMETER)} /* THIRD COMPONENT */>
+                <CardContent>
+                    <Typography color='textSecondary'>THIRD THING</Typography> 
+                    <Typography variant='h5'>
+                        <CountUp start={0} end={1000000} duration={2.5} separator=','/>
+                    </Typography>
+                </CardContent>
+            </Grid>
+            <Grid item component={Card} xs={5} md={5} className={names(styles.card, styles.FORTHPARAMTER)} /* FORTH COMPONENT */>
+                <CardContent>
+                    <Typography color='textSecondary'>FORTH THING</Typography> 
+                    <Typography variant='h5'>
+                        <CountUp start={0} end={1000000} duration={2.5} separator=','/>
                     </Typography>
                 </CardContent>
             </Grid>
