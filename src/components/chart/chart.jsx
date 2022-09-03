6fc29ed1d991f8127089dc898321268653f01095
JasonEide <jeide15@yahoo.com>
2022-09-03 15:48:40 -0400

    implemented frontend pie chart

--- src/components/chart/chart.jsx
+++ src/components/chart/chart.jsx
@@ -1,1 +1,24 @@
+import React, {useState, useEffect} from 'react';
 import {Pie} from 'react-chartjs-2';
+import styles from './chart.module.css';
+
+export default function Charts() {
+    const [currentData, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setData(await FETCHDATAHERE());
+        }
+        setInterval(() => {
+            fetchData();
+        }, 1000);
+    }, [])
+
+    const pieChart = (
+        currentData.length
+        ? (
+            <Pie>
+            </Pie>
+        ) : null
+    )
+    };
